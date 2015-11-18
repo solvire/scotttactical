2c403a021906996c7238be9eb905509a4d0c3ab0
solvire <stevenjscott@gmail.com>
2015-11-18 02:31:19 -0800

    Minifying the files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp')
 var less = require('gulp-less')
 var path = require('path');
 var mini = require('gulp-minify-css');
+var miniH = require('gulp-minify-html');
 
 gulp.task('less', function() {
 	return gulp
@@ -12,4 +13,16 @@ gulp.task('less', function() {
 		.pipe(gulp.dest('./static/css'));
 });
 
-gulp.task('default', [ 'less' ])
+
+gulp.task('minify-html', function() {
+	var opts = {
+		conditionals : false,
+		spare : true
+	};
+
+	return gulp.src('./public/**/*.html')
+		.pipe(miniH(opts))
+		.pipe(gulp.dest('./public/'));
+});
+
+gulp.task('default', [ 'less', 'minify-html' ])
